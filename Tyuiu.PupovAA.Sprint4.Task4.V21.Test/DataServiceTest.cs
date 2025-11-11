2938a792452b1f7c5a7ab23f687c47c06e88aa53
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-11-12 03:24:15 +0500

    Task4

---
+++ Tyuiu.PupovAA.Sprint4.Task4.V21.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PupovAA.Sprint4.Task4.V21.Lib;
+namespace Tyuiu.PupovAA.Sprint4.Task4.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] data = new int[,] { { 3, 6, 4, 6, 6 }, { 7, 5, 5, 5, 8 }, { 8, 3, 6, 7, 7 }, { 5, 3, 8, 4, 5 }, { 3, 7, 4, 3, 8 } };
+            var res = ds.Calculate(data);
+            Assert.AreEqual(68, res);
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint4.Task4.V21/Program.cs
@@ -0,0 +1,54 @@
+﻿using Tyuiu.PupovAA.Sprint4.Task4.V21.Lib;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #4 | Выполнил: Пупов А. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                          *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #21                                                              *");
+        Console.WriteLine("* Выполнил: Пупов Артём Андреевич  | ИБКСб-25-1                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        
+        Console.WriteLine("Впишите число строк массива:");
+        int len = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Впишите число столбцов массива:");
+        int nel = Convert.ToInt32(Console.ReadLine());
+        int[,] nums = new int[len,nel];
+        
+
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < nel; j++)
+            {
+                Console.WriteLine("Впишите элемент массива");
+                nums[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Массив");
+        for (int i = 0; i < len; i++)
+        {
+            for (int j = 0; j < nel; j++)
+            {
+                Console.WriteLine($"{nums[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Результат:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int narray = ds.Calculate(nums);
+        Console.WriteLine("результат сложения четных элементов массива = " + narray);
+        Console.ReadLine();
+    }
+}
