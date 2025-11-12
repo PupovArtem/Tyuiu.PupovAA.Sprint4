f73c24bdb8e5ce4efbb56a96ff2816a3a8638a99
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-11-12 10:01:06 +0500

    Task7

---
+++ Tyuiu.PupovAA.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.PupovAA.Sprint4.Task7.V28.Lib;
+namespace Tyuiu.PupovAA.Sprint4.Task7.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 5;
+            int cols = 3;
+            int[,] data = new int[rows, cols];
+            string str = "623351179845632";
+            DataSrervice ds = new DataSrervice();
+            int res =ds.Calculate(rows, cols,str);
+            Assert.AreEqual(4608,res);
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint4.Task7.V28/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.PupovAA.Sprint4.Task7.V28.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int rows = 5;
+        int cols = 3;
+        int[,] data = new int[rows, cols];
+        string str = "623351179845632";
+        DataSrervice ds = new DataSrervice();
+        int res = ds.Calculate(rows, cols, str);
+        Console.WriteLine(res);
+    }
+}
